b8b6720ca4d81817185b02caf64561fc67fbf5aa
holyalin <amalinazahranip@gmail.com>
2024-05-29 21:52:10 +0700

    checkpoint, menambahkan fitur edit profile

--- vote.ly_be/index.js
+++ vote.ly_be/index.js
@@ -1,3 +1,16 @@
-const express = require('express'); const app = express(); 
-const port = 3000; app.get('/', (req, res) => res.send('Hello World!')); 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+
+// Atur direktori 'vote.ly_fe' sebagai direktori statis
+app.use(express.static(path.join(__dirname, '..', 'vote.ly_fe')));
+
+// Route utama yang menyajikan 'index.html'
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'vote.ly_fe', 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
