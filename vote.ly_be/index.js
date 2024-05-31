a8568676f479c2ba6fe305b0d373e23994dd781a
arsyadal <arsyadal@student.telkomuniversity.ac.id>
2024-05-31 22:15:12 +0700

    checkpoint

--- vote.ly_be/index.js
+++ vote.ly_be/index.js
@@ -1,16 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const port = 3000;
+const path = require("path");
+const port = 3030;
 
 // Atur direktori 'vote.ly_fe' sebagai direktori statis
-app.use(express.static(path.join(__dirname, '..', 'vote.ly_fe')));
+app.use(express.static(path.join(__dirname, "..", "vote.ly_fe")));
 
 // Route utama yang menyajikan 'index.html'
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'vote.ly_fe', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "vote.ly_fe", "index.html"));
 });
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
